b90e0e0bb893c290f447cc3f25bab8bc963f6702
vinayyadav3016 <vinayyadav3016@gmail.com>
2017-02-14 23:11:16 +0530

    -- Fixed serial0 atmega328 and atmega8 conversion -- Added serial1 interface

--- include/serial1.hpp
+++ include/serial1.hpp
@@ -1,13 +1,13 @@
-/*! \file serial0.hpp
+/*! \file serial1.hpp
  */
-#ifndef __SERIAL0_HPP__
-#define __SERIAL0_HPP__
+#ifndef __SERIAL1_HPP__
+#define __SERIAL1_HPP__
 
 #include <serial_base.hpp>
 
 /*! \class
  */
-class Serial0 : public SerialBase {
+class Serial1 : public SerialBase {
 	/*!
 	 */
 	protected:
@@ -15,45 +15,45 @@ class Serial0 : public SerialBase {
 		 */
 		inline void initUSART(const int baudrate) const {
 			uint16_t	ubbr_value = (F_CPU/(baudrate*16UL))-1;
-			UBRR0L = ubbr_value;
-			UBRR0H = ubbr_value>>8;
-			UCSR0C =(1<<UCSZ01)|(1<<UCSZ00);
+			UBRR1L = ubbr_value;
+			UBRR1H = ubbr_value>>8;
+			UCSR1C =(1<<UCSZ11)|(1<<UCSZ10);
 		}
 		/*!
 		 */
 		inline void startUSART() const {
-			UCSR0B|=(1<<RXEN0)|(1<<TXEN0)|(1<<RXCIE0)|(1<<TXCIE0)|(1<<UDRIE0);
+			UCSR1B|=(1<<RXEN1)|(1<<TXEN1)|(1<<RXCIE1)|(1<<TXCIE1)|(1<<UDRIE1);
 		}
 		/*!
 		 */
 		inline void stopTransmission() {
-			UCSR0B&=~(1<<UDRIE0);
+			UCSR1B&=~(1<<UDRIE1);
 		}
 		/*!
 		 */
 		void stopUSART() {
 			stopTransmission();
-			UBRR0H=0x00;
-			UBRR0L=0x00;
-			UCSR0B=0x00;
-			UCSR0C=0x00;
+			UBRR1H=0x00;
+			UBRR1L=0x00;
+			UCSR1B=0x00;
+			UCSR1C=0x00;
 		}
 	public:
 		/*!
 		 */
-		Serial0(int baudrate,uint8_t rec_size,uint8_t trans_size):SerialBase(rec_size,trans_size) {
+		Serial1(int baudrate,uint8_t rec_size,uint8_t trans_size):SerialBase(rec_size,trans_size) {
 			initUSART(baudrate);
 			startUSART();
 		}
 		/*!
 		 */
-		~Serial0() {
+		~Serial1() {
 			stopUSART();
 		}
 		/*!
 		 */
 		inline void startTransmission() const {
-			UCSR0B|=(1<<UDRIE0);
+			UCSR1B|=(1<<UDRIE1);
 		}
 		/*!
 		 */
@@ -61,7 +61,7 @@ class Serial0 : public SerialBase {
 			if(_transmitter.getReadBuffLength()>0) {
 				uint8_t data=0;
 				_transmitter.readByte(data);
-				UDR0 = data;
+				UDR1 = data;
 			}
 			else {
 				stopTransmission();
@@ -82,13 +82,13 @@ class Serial0 : public SerialBase {
 
 /*!
  */
-Serial0* SERIAL_NAME;//(BAUD_RATE,RX_BUFF,TX_BUFF);
+Serial1* SERIAL_NAME;//(BAUD_RATE,RX_BUFF,TX_BUFF);
 
 /*!
  */
-Serial0* getSerialPort0(uint16_t baud_rate,uint16_t rx_buff,uint16_t tx_buff) {
+Serial1* getSerialPort1(uint16_t baud_rate,uint16_t rx_buff,uint16_t tx_buff) {
 	if(SERIAL_NAME==NULL)
-		SERIAL_NAME = new Serial0(baud_rate,rx_buff,tx_buff);
+		SERIAL_NAME = new Serial1(baud_rate,rx_buff,tx_buff);
 	return SERIAL_NAME;
 }
 
@@ -98,40 +98,28 @@ void destroySerialPort0() {
 	delete SERIAL_NAME;
 }
 
-#ifndef USART0_TX_vect
-#define USART0_TX_vect USART_TX_vect
-#endif
-
-#ifndef USART0_RX_vect
-#define USART0_RX_vect USART_RX_vect
-#endif
-
-#ifndef USART0_UDRE_vect
-#define USART0_UDRE_vect USART_UDRE_vect
-#endif
-
-#ifdef USART0_UDRE_vect
+#ifdef USART1_UDRE_vect
 
 /*!
  */
-ISR(USART0_UDRE_vect) {
+ISR(USART1_UDRE_vect) {
 	SERIAL_NAME->doUDRISR();
 }
 
 /*!
  */
-ISR(USART0_RX_vect) {
-	const uint8_t data = UDR0;
+ISR(USART1_RX_vect) {
+	const uint8_t data = UDR1;
 	SERIAL_NAME->doRXISR(data);
 }
 
 /*!
  */
-ISR(USART0_TX_vect) {
+ISR(USART1_TX_vect) {
 	SERIAL_NAME->doTXISR();
 }
 #elif
 Error "No Interrupt vector defined how"
 #endif
 
-#endif //__SERIAL0_HPP___
+#endif //__SERIAL1_HPP___
